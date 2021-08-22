4e8db0081e6d5918ef7c0cb015a194b7cbc393b3
Mohammed Hamouda <m97.business@gmail.com>
2021-08-23 01:36:35 +0200

    filter

--- src/app/shared/sidebar/menu-items.ts
+++ src/app/shared/sidebar/menu-items.ts
@@ -53,31 +53,6 @@ export const ROUTES: RouteInfo[] = [
   },
   {
     path: '',
-    title: 'الارباح',
-    icon: 'far fa-money-bill-alt',
-    class: 'has-arrow',
-    extralink: false,
-    submenu: [
-      {
-        path: '/profits/all',
-        title: 'عرض',
-        icon: '',
-        class: '',
-        extralink: false,
-        submenu: []
-      },
-      {
-        path: '/profits/add',
-        title: 'اضافة',
-        icon: '',
-        class: '',
-        extralink: false,
-        submenu: []
-      },
-    ]
-  },
-  {
-    path: '',
     title: 'سلايدر',
     icon: 'fas fa-images',
     class: 'has-arrow',
